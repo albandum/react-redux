6909ee3dd59356492300906cc2938edd6163cc7a
Alban Dumouilla <alban.dumouilla@gmail.com>
2017-08-21 12:29:37 +0200

    Loginform card OK

--- manager/src/app.js
+++ manager/src/app.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 import firebase from 'firebase';
 
 import reducers from './reducers';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
   componentWillMount() {
@@ -24,9 +25,7 @@ class App extends Component {
     return (
       <Provider store={createStore(reducers)}>
         <View>
-          <Text>
-            Hello !
-          </Text>
+          <LoginForm />
         </View>
       </Provider>
     );
